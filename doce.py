aae8f148c0fe2d551343a8013773e380d134c2f9
veirosp <paolaveiros@gmail.com>
2025-06-03 15:06:07 -0300

    Update doce.py

--- doce.py
+++ doce.py
@@ -11,11 +11,11 @@ st.image("https://ofuxico.com.br/wp-content/uploads/2023/05/barata-MET-GALA.jpg"
     output_format="auto",
     use_container_width=False)
   
-numero = st.text_input("https://ofuxico.com.br/wp-content/uploads/2023/05/barata-MET-GALA.jpg",
-  label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible", icon=None)("Digite aqui quantas saias de filó a barata tem")
+st.text_input = ("Digite aqui quantas saias de filó a barata tem",label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible", icon=None)
+)
 
-if numero == "1":
+if st.text_input == "1":
   st.write("Acertou, ela tem é uma só")
   st.video(data, format="video/mp4", start_time=0, *, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
 else: 
-  print("Errou, era mentira da barata")
+  st.write("Errou, era mentira da barata")
