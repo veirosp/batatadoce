384a281b3f86aefe079d56da74e5fdef8062c40f
veirosp <paolaveiros@gmail.com>
2025-06-03 14:57:22 -0300

    Create doce.py

---
+++ doce.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Quantas saias de filó a barata tem?")
+
+st.image("https://ofuxico.com.br/wp-content/uploads/2023/05/barata-MET-GALA.jpg",
+         width=None,
+    use_column_width=None,
+    clamp=False,
+    channels="RGB",
+    output_format="auto",
+    use_container_width=False)
+  
+numero = st.text_input("https://ofuxico.com.br/wp-content/uploads/2023/05/barata-MET-GALA.jpg",
+  label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible", icon=None)("Digite aqui quantas saias de filó a barata tem")
+
+if numero == "1":
+  st.write("Acertou, ela tem é uma só")
+  st.video(data, format="video/mp4", start_time=0, *, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
+else: 
+  print("Errou, era mentira da barata")
